62a0809f0262ca0ca4975d1c6d49ae7961a8d2aa
ighoakponah <ighoakponah@IGHOAKPONAH7872>
2019-01-05 17:25:45 +0000

    Add project files.

---
+++ CaregiverTests/tstStaff.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CaregiverTests
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStaff AStaff = new clsStaff();
+        }
+    }
+}
