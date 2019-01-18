edbc40b59fe5a7ba85c6a80dfbeba6d0d25f87c2
Igho <ighoakponah@yahoo.com>
2019-01-18 17:31:01 +0000

    Validation class added

---
+++ CaregiverTests/tstValidation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CaregiverTests
+{
+    [TestClass]
+    public class tstValidation
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsValidation Valid = new clsValidation();
+
+            //Check to ensure that the class exists
+            Assert.IsNotNull(Valid);
+        }
+    }
+}
