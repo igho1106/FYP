c3f3875927007fa4133db6718880a912dba33b22
Igho <ighoakponah@yahoo.com>
2019-01-18 17:14:56 +0000

    added get/set methods to the classes

--- ClassLibrary/clsClient.cs
+++ ClassLibrary/clsClient.cs
@@ -6,13 +6,154 @@ namespace ClassLibrary
     {
         //Client Properties
         public bool Active { get; set; }
-        public int ManagerID { get; set; }
-        public DateTime DOB { get; set; }
-        public string EmailAddress { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Gender { get; set; }
-        public string HomeAddress { get; set; }
-        public string CareRequirement { get; set; }
+
+
+        //private data member for the ClientID property
+        private int mClientID;
+        //private data member for the DOB property
+        private DateTime mDOB;
+        //private data member for the EmailAddress property
+        private string mEmailAddress;
+        //private data member for the FirstName property
+        private string mFirstName;
+        //private data member for the LastName property
+        private string mLastName;
+        //private data member for the Gender property
+        private string mGender;
+        //private data member for the HomeAddress property
+        private string mHomeAddress;
+        //private data member for the PhoneNo property
+        private string mPhoneNo;
+        //private data member for the PhoneNo property
+        private string mCareRequirement;
+
+
+
+
+
+        public int ClientID
+        {
+            get
+            {
+                //return the private data
+                return mClientID;
+            }
+            set
+            {
+                //set the value of the private data member
+                mClientID = value;
+            }
+        }
+
+        public DateTime DOB
+        {
+            get
+            {
+                //return the private data
+                return mDOB;
+            }
+            set
+            {
+                //set the value of the private data member
+                mDOB = value;
+            }
+        }
+
+        public string EmailAddress
+        {
+            get
+            {
+                //return the private data
+                return mEmailAddress;
+            }
+            set
+            {
+                //set the value of the private data member
+                mEmailAddress = value;
+            }
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                //return the private data
+                return mFirstName;
+            }
+            set
+            {
+                //set the value of the private data member
+                mFirstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                //return the private data
+                return mLastName;
+            }
+            set
+            {
+                //set the value of the private data member
+                mLastName = value;
+            }
+        }
+        public string Gender
+        {
+            get
+            {
+                //return the private data
+                return mGender;
+            }
+            set
+            {
+                //set the value of the private data member
+                mGender = value;
+            }
+        }
+
+        public string HomeAddress
+        {
+            get
+            {
+                //return the private data
+                return mHomeAddress;
+            }
+            set
+            {
+                //set the value of the private data member
+                mHomeAddress = value;
+            }
+        }
+
+        public string CareRequirement
+        {
+            get
+            {
+                //return the private data
+                return mCareRequirement;
+            }
+            set
+            {
+                //set the value of the private data member
+                mCareRequirement = value;
+            }
+        }
+
+        public string PhoneNo
+        {
+            get
+            {
+                //return the private data
+                return mPhoneNo;
+            }
+            set
+            {
+                //set the value of the private data member
+                mPhoneNo = value;
+            }
+        }
     }
 }
