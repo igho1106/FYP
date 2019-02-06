cff5fc0b89de2ea424561db1ca01826daa70e7ba
Igho <ighoakponah@yahoo.com>
2019-02-06 13:30:28 +0000

    More Tests

--- CaregiverTests/tstManager.cs
+++ CaregiverTests/tstManager.cs
@@ -141,11 +141,121 @@ namespace CaregiverTests
         [TestMethod]
         public void FindMethodOK()
         {
-            clsManager AUser = new clsManager();
+            clsManager AManager = new clsManager();
             Boolean Found = false;
             int UserID = 1;
-            Found = AUser.Find(UserID);
+            Found = AManager.Find(UserID);
             Assert.IsTrue(Found);
+        }
+
+        [TestMethod]
+        public void TestUserFirstNameFound()
+        {
+            clsManager AManager = new clsManager();
+            Boolean Found = false;
+            Boolean OK = true;
+            int UserID = 1;
+            Found = AManager.Find(UserID);
+
+            if (AManager.FirstName != "Igho")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        public void TestUserLastNameFound()
+        {
+            clsManager AManager = new clsManager();
+            Boolean Found = false;
+            Boolean OK = true;
+            int UserID = 1;
+            Found = AManager.Find(UserID);
+
+            if (AManager.FirstName != "Akponah")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        public void TestUserGenderFound()
+        {
+            clsManager AManager = new clsManager();
+            Boolean Found = false;
+            Boolean OK = true;
+            int UserID = 1;
+            Found = AManager.Find(UserID);
+
+            if (AManager.FirstName != "Male")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestUserPhoneNoFound()
+        {
+            clsManager AManager = new clsManager();
+            Boolean Found = false;
+            Boolean OK = true;
+            int CustomerID = 1;
+            Found = AManager.Find(CustomerID);
+
+            if (AManager.PhoneNo != "07397924314")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestUserHomeAddressFound()
+        {
+            clsManager AManager = new clsManager();
+            Boolean Found = false;
+            Boolean OK = true;
+            int CustomerID = 1;
+            Found = AManager.Find(CustomerID);
+
+            if (AManager.HomeAddress != "6 Cavendish Mews")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestUserEmailAddressFound()
+        {
+            clsManager AManager = new clsManager();
+            Boolean Found = false;
+            Boolean OK = true;
+            int CustomerID = 1;
+            Found = AManager.Find(CustomerID);
+
+            if (AManager.EmailAddress != "ighoakponah@123.com")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestUserDOBFound()
+        {
+            clsManager AManager = new clsManager();
+            Boolean Found = false;
+            Boolean OK = true;
+            int CustomerID = 1;
+            Found = AManager.Find(CustomerID);
+
+            if (AManager.DOB != Convert.ToDateTime("11/06/1989"))
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
         }
     }
 }
