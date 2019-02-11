4f207c1a6e13b066af82a844165747494ae6596d
Igho <ighoakponah@yahoo.com>
2019-02-11 14:18:11 +0000

    more tests

---
+++ CaregiverTests/tstUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CaregiverTests
+{
+    [TestClass]
+    public class tstUser
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
