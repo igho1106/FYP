291cf775e93434029f8ba1697ab7abdb88722ac4
Igho <ighoakponah@yahoo.com>
2019-02-14 16:25:58 +0000

    Collection Class added

--- CaregiverTests/tstUserCollection.cs
+++ CaregiverTests/tstUserCollection.cs
@@ -1,1 +1,151 @@
-﻿
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace CaregiverTests
+{
+    [TestClass]
+    public class tstUserCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsUserCollection AllUsers = new clsUserCollection();
+            //test to see that it exists
+            Assert.IsNotNull(AllUsers);
+        }
+
+
+        [TestMethod]
+        public void UserListOK()
+        {
+            //create an instance of the class we want to create
+            clsUserCollection AllCustomers = new clsUserCollection();
+            //create some test data to assin to the property
+            //in this case the data needs to be a list of objects
+            List<clsUser> TestList = new List<clsUser>();
+            //add an item to the list
+            //create the item of the test data
+            clsUser TestItem = new clsUser();
+            //set its properties 
+            TestItem.UserID = 2;
+            TestItem.FirstName = "John";
+            TestItem.LastName = "Doe";
+            TestItem.Gender = "Male";
+            TestItem.EmailAddress = "abc@123.com";
+            TestItem.HomeAddress = "8 Millstone Lane";
+            TestItem.DOB = DateTime.Now.Date;
+            TestItem.PhoneNo = "01234567891";
+
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllCustomers.UserList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllCustomers.UserList, TestList);
+        }
+
+        [TestMethod]
+        public void ThisUserPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsUserCollection AllCustomers = new clsUserCollection();
+            //create some test data to assin to the property
+            clsUser TestUser = new clsUser();
+            //set its properties 
+            TestUser.UserID = 2;
+            TestUser.FirstName = "John";
+            TestUser.LastName = "Doe";
+            TestUser.Gender = "Male";
+            TestUser.EmailAddress = "abc@123.com";
+            TestUser.HomeAddress = "8 Millstone Lane";
+            TestUser.DOB = DateTime.Now.Date;
+            TestUser.PhoneNo = "01234567891";
+
+            //assign the data to the property
+            AllCustomers.ThisUser = TestUser;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllCustomers.ThisUser, TestUser);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            //create an instance of the class we want to create
+            clsUserCollection AllUsers = new clsUserCollection();
+            //create some test data to assin to the property
+            //in this case the data needs to be a list of objects
+            List<clsUser> TestList = new List<clsUser>();
+            //add an item to the list
+            //create the item of the test data
+            clsUser TestItem = new clsUser();
+            //set its properties 
+            TestItem.UserID = 2;
+            TestItem.FirstName = "John";
+            TestItem.LastName = "Doe";
+            TestItem.Gender = "Male";
+            TestItem.EmailAddress = "abc@123.com";
+            TestItem.HomeAddress = "8 Millstone Lane";
+            TestItem.DOB = DateTime.Now.Date;
+            TestItem.PhoneNo = "01234567891";
+
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllUsers.UserList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllUsers.Count, TestList.Count);
+        }
+
+        [TestMethod]
+        public void AddUserMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsUserCollection AllUsers = new clsUserCollection();
+            //create a the item of the test data
+            clsUser TestItem = new clsUser();
+            //var to store the primary key
+            Int32 PrimaryKey = 1;
+            //set its properties 
+            TestItem.UserID = 2;
+            TestItem.FirstName = "John";
+            TestItem.LastName = "Doe";
+            TestItem.Gender = "Male";
+            TestItem.EmailAddress = "abc@123.com";
+            TestItem.HomeAddress = "8 Millstone Lane";
+            TestItem.DOB = DateTime.Now.Date;
+            TestItem.PhoneNo = "01234567891";
+
+            //set ThisCustomer to the test data
+            AllUsers.ThisUser = TestItem;
+            //add the record
+            PrimaryKey = AllUsers.AddUserMethod();
+            //set the primary key of the test data
+            TestItem.UserID = PrimaryKey;
+            //find the record
+            AllUsers.ThisUser.Find(PrimaryKey);
+            //test to see that the two values are the same
+            Assert.AreEqual(AllUsers.ThisUser, TestItem);
+
+        }
+
+        //DELETE TEST METHOD
+
+        //UPDATE TEST METHOD
+
+        [TestMethod]
+        public void FilterByEmailAddressMethodOK()
+        {
+            //create an instance of the class containing unfiltered results
+            clsUserCollection AllUsers = new clsUserCollection();
+            //create an instance of the filtered data
+            clsUserCollection FilteredCustomers = new clsUserCollection();
+            //apply a blank string (should return all records);
+            FilteredCustomers.FilterByEmailAddress("");
+            //test to see that the two values are the same
+            Assert.AreEqual(AllUsers.Count, FilteredCustomers.Count);
+        }
+    }
+}
--- ClassLibrary/clsUserCollection.cs
+++ ClassLibrary/clsUserCollection.cs
@@ -1,17 +1,139 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace ClassLibrary
 {
     public class clsUserCollection
     {
-        public List<clsUser> UserList { get; set; }
-        public clsUser ThisUser { get; set; }
-        public int Count { get; set; }
+        //private data member thisCustomer
+        clsUser mThisUser = new clsUser();
 
-        public int Add()
+        //private data member for the list
+        List<clsUser> mUserList = new List<clsUser>();
+        public List<clsUser> UserList
         {
-            throw new NotImplementedException();
+            get
+            { //return the private data
+                return mUserList;
+            }
+            set
+            {
+                //set the private data
+                mUserList = value;
+            }
+        }
+        public clsUser ThisUser
+        {
+            get
+            {
+                //return the private data
+                return mThisUser;
+            }
+            set
+            {
+                //set the private data
+                mThisUser = value;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                //return the count of the list
+                return mUserList.Count;
+            }
+            set
+            {
+                //TBC
+            }
+        }
+
+        //constructor for the class
+        public clsUserCollection()
+        {
+            //object for data connection
+            clsDataConnection DB = new clsDataConnection();
+            //execute the stored procedure
+            DB.Execute("sproc_tblUser_SelectALL");
+            //populate the array list with the data table
+            PopulateArray(DB);
+
+        }
+
+
+
+        public int AddUserMethod()
+        {
+            //adds a new recoord to the dayanase basaed on the values of mThisCustomer
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored procedure
+
+            DB.AddParameter("@FirstName", mThisUser.FirstName);
+            DB.AddParameter("@LastName", mThisUser.LastName);
+            DB.AddParameter("@Gender", mThisUser.Gender);
+            DB.AddParameter("@EmailAddress", mThisUser.EmailAddress);
+            DB.AddParameter("@HomeAddress", mThisUser.HomeAddress);
+            DB.AddParameter("@DOB", mThisUser.DOB);
+            DB.AddParameter("@PhoneNo", mThisUser.PhoneNo);
+            
+
+            //execute query to return the primary key value
+            return DB.Execute("sproc_tblUser_Insert");
+        }
+
+        public void Delete()
+        {
+            //deletes the record pointed to by thisAddress
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored procedure
+            DB.AddParameter("@UserID", mThisUser.UserID);
+            //execute the stored procedure
+            DB.Execute("sproc_tblUser_Delete");
+        }
+
+
+        void PopulateArray(clsDataConnection DB)
+        {
+            //populates the array list based on the data table in the parameter DB
+            //var for the index
+            Int32 Index = 0;
+            //var to store the record count
+            Int32 RecordCount;
+            //get the count of records
+            RecordCount = DB.Count;
+            //clear the private array list
+            mUserList = new List<clsUser>();
+            //while there are records to process
+            while (Index < RecordCount)
+            {
+                //create a blank address
+                clsUser AUser = new clsUser();
+                //read in the fields from the current record
+
+                AUser.FirstName = Convert.ToString(DB.DataTable.Rows[Index]["FirstName"]);
+                AUser.LastName = Convert.ToString(DB.DataTable.Rows[Index]["LastName"]);
+                AUser.Gender = Convert.ToString(DB.DataTable.Rows[Index]["Gender"]);
+                AUser.EmailAddress = Convert.ToString(DB.DataTable.Rows[Index]["EmailAddress"]);
+                AUser.HomeAddress = Convert.ToString(DB.DataTable.Rows[Index]["HomeAddress"]);
+                AUser.DOB = Convert.ToDateTime(DB.DataTable.Rows[Index]["DOB"]);
+                AUser.PhoneNo = Convert.ToString(DB.DataTable.Rows[Index]["PhoneNo"]);
+                
+                //add the record to the private data mamber
+                mUserList.Add(AUser);
+                //point at the next record
+                Index++;
+            }
+
+        }
+
+        public void FilterByEmailAddress(string EmailAddress)
+        {
+            //TBC
+            return;
         }
     }
 }
